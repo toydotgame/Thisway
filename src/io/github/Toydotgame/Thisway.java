cd991a040ab3a5c322de323446a8db5e7df20313
Toydotgame <MatthewJPersonalAcct@outlook.com>
2020-12-26 18:54:30 +1000

    Testing player location detector

--- src/io/github/Toydotgame/Thisway.java
+++ src/io/github/Toydotgame/Thisway.java
@@ -1,5 +1,6 @@
 package io.github.Toydotgame;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,6 +11,12 @@ public class Thisway implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
+			
+			
+			
+			/* 
+			 * PLAYER FACING DATECTOR
+			 * */
 			float yaw = player.getEyeLocation().getYaw();
 			sender.sendMessage("Yaw is: " + String.valueOf(yaw));
 			
@@ -25,7 +32,27 @@ public class Thisway implements CommandExecutor {
 		    } else if(yaw < 315) {
 		    	sender.sendMessage("EAST");
 		    }
+		    /* 
+		     * END PLAYER FACING DETECTOR
+		     * */
+		    
+		    
 			
+		    /* 
+		     * PLAYER POSITION IN WORLD DETECTOR
+		     * */
+		     Location location = player.getLocation();
+		     int playerX = location.getBlockX();
+		     int playerY = location.getBlockY();
+		     int playerZ = location.getBlockZ();
+		     
+		     sender.sendMessage("Position: " + playerX + ", " + playerY + ", " + playerZ + " (X, Y, Z)");
+		    /* 
+		     * END PLAYER POSITION DETECTOR
+		     * */
+		    
+		    
+		    
 			return true;
 		} else {
 			System.out.println("Command Failed!");
