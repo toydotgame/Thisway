4bac329a55bb7a7bf5577caf51d6f044f12e66b3
toydotgame <toydotgame@gmail.com>
2021-01-21 11:20:53 +1000

    Made some datastorage variables null by default

--- src/io/github/Toydotgame/DataStorage.java
+++ src/io/github/Toydotgame/DataStorage.java
@@ -3,8 +3,8 @@ package io.github.Toydotgame;
 // Ah, so simple, yet so helpful.
 
 public class DataStorage {
-	public static boolean debug = false;
-	public static String facing = "";
+	public static boolean debug;
+	public static String facing;
 	
 	public static int xModDistance = 0;
 	public static int zModDistance = 0;
