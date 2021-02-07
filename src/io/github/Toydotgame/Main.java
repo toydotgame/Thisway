a6fded75a12ce023f728d878d7a9c0c8aa89e7eb
toydotgame <toydotgame@gmail.com>
2021-02-07 14:53:54 +1000

    trying to fix fatal version bug, not good so far

--- src/io/github/Toydotgame/Main.java
+++ src/io/github/Toydotgame/Main.java
@@ -6,18 +6,16 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		new UpdateChecker(this, 87115).getVersion(version -> { // Huge thanks to the great code on the SpigotMC wiki: https://www.spigotmc.org/wiki/creating-an-update-checker-that-checks-for-updates/
-			DataStorage.version = this.getDescription().getVersion();
-			
 			if(this.getDescription().getVersion().equalsIgnoreCase(version)) {
-				DataStorage.isUpToDate = true;
-				this.getCommand("thisway").setExecutor(new Thisway()); // Plugin command code, loaded in both cases.
 				System.out.print("[Thisway] Plugin successfully loaded.");
+				DataStorage.isUpToDate = true;
 			} else {
+				System.out.print("[Thisway] Please update Thisway! (Plugin loading anyway)");
 				DataStorage.isUpToDate = false;
-				this.getCommand("thisway").setExecutor(new Thisway());
-				System.out.print("[Thisway] Please update Thisway! (Plugin loaded anyway)");
 			}
+			DataStorage.version = this.getDescription().getVersion();
 		});
+		this.getCommand("thisway").setExecutor(new Thisway());
 	}
 
 	@Override
