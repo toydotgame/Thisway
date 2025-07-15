bac8c0edfaf70e078a4967f637aefd051d82a52c
toydotgame <toydotgame@gmail.com>
2025-07-15 18:27:09 +1000

    Package move for refactor

---
+++ src/net/toydotgame/Thisway/Thisway.java
@@ -0,0 +1,8 @@
+package net.toydotgame.Thisway;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Thisway extends JavaPlugin {
+	// No need to add a custom constructor/actually extend JavaPlugin. Just take
+	// an instance of class Thisway to be a plain ol' JavaPlugin
+}
