985626bae371f11312af7c46cd0efaf34855e0a0
toydotgame <toydotgame@gmail.com>
2025-07-15 18:32:07 +1000

    Simple `onEnable()` message
    This addresses #6 also. I'll use a logger from now on I swear!

--- src/net/toydotgame/Thisway/Thisway.java
+++ src/net/toydotgame/Thisway/Thisway.java
@@ -5,4 +5,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Thisway extends JavaPlugin {
 	// No need to add a custom constructor/actually extend JavaPlugin. Just take
 	// an instance of class Thisway to be a plain ol' JavaPlugin
+	
+	/**
+	 * Log a simple message to follow the Spigot logs that only say that they've
+	 * <i>begun</i> enabling Thisway. {@inheritDoc}.
+	 */
+	@Override
+	public void onEnable() {
+		this.getLogger().info("Enabled!");
+	}
 }
