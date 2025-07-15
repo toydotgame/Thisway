025431dcda094f2b10f06c06299e45f009791f38
toydotgame <toydotgame@gmail.com>
2025-07-15 23:05:42 +1000

    Some comments

--- src/net/toydotgame/Thisway/Thisway.java
+++ src/net/toydotgame/Thisway/Thisway.java
@@ -1,5 +1,7 @@
 package net.toydotgame.Thisway;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -16,9 +18,26 @@ public final class Thisway extends JavaPlugin {
 	 */
 	@Override
 	public void onEnable() {
-		UpdateChecker.checkForUpdates(this);
+		UpdateChecker.checkForUpdates(this); // Not used after this point
 		Configurator.loadConfig(this);
 		
 		this.getLogger().info("Enabled!");
+	}
+	
+	/**
+	 * Main command execution logic.
+	 * TODO: Seperate class and .setExecutor()? Or just do parsing and
+	 * validation here and pass on teleportation and other exeuction to other
+	 * classes (probably do this)?
+	 * @return {@code true} for syntatically-correct inputs, {@code false} for
+	 * those which are not (thus prints usage)
+	 */
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// sender - object associated with command sender (instanceof Player desired)
+		// command - command object corresponding to entry in plugin.yml
+		// label - alias used (/thisway or /tw)
+		// args[] - what it says on the tin
+		return false;
 	}
 }
